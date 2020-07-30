45739afb7a279f62cdc908bc3324daf6480cf8f1
Akshay Jinde <akshayjinde6@gmail.com>
2020-07-30 17:47:13 +0530

    Devotional Added

---
+++ app/src/main/java/com/pravingaikwad/solapurcityguide/Devotional.java
@@ -0,0 +1,14 @@
+package com.pravingaikwad.solapurcityguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Devotional extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devotional);
+    }
+}
