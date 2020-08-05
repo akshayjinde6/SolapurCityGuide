8caa9a92cec57d532f036b5d3b439607066f80e9
Akshay Jinde <akshayjinde6@gmail.com>
2020-08-05 22:10:21 +0530

    Devotional Screen

--- app/src/main/java/com/pravingaikwad/solapurcityguide/Devo_info.java
+++ app/src/main/java/com/pravingaikwad/solapurcityguide/Devo_info.java
@@ -2,13 +2,60 @@ package com.pravingaikwad.solapurcityguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Devo_info extends AppCompatActivity {
+
+    TextView name, location;
+    LinearLayout sid, tul, pan, gan, akka, bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devo_info);
+
+        name = findViewById(R.id.name);
+        location = findViewById(R.id.location);
+        sid = findViewById(R.id.sid);
+        tul = findViewById(R.id.tul);
+        pan = findViewById(R.id.pan);
+        gan = findViewById(R.id.gan);
+        akka = findViewById(R.id.akka);
+        bar = findViewById(R.id.bar);
+        Intent i = getIntent();
+
+        String dev = i.getStringExtra("dev");
+        if (dev.equals("sid")) {
+            sid.setVisibility(View.VISIBLE);
+        }
+        if (dev.equals("tul")) {
+            name.setText(R.string.tuljapur_name);
+            location.setText(R.string.tuljapur_location);
+            tul.setVisibility(View.VISIBLE);
+        }
+        if (dev.equals("pan")) {
+            name.setText(R.string.pandharpur_temple_name);
+            location.setText(R.string.pan_location);
+            pan.setVisibility(View.VISIBLE);
+        }
+        if (dev.equals("gan")) {
+            name.setText(R.string.gangapur_temple_name);
+            location.setText(R.string.gan_location);
+            gan.setVisibility(View.VISIBLE);
+        }
+        if (dev.equals("akka")) {
+            name.setText(R.string.akkalkot_temple_name);
+            location.setText(R.string.akka_location);
+            tul.setVisibility(View.VISIBLE);
+        }
+        if (dev.equals("bar")) {
+            name.setText(R.string.barshi_temple_name);
+            location.setText(R.string.bar_location);
+            tul.setVisibility(View.VISIBLE);
+        }
     }
 }
--- app/src/main/java/com/pravingaikwad/solapurcityguide/Devotional.java
+++ app/src/main/java/com/pravingaikwad/solapurcityguide/Devotional.java
@@ -9,18 +9,35 @@ import android.view.View;
 
 public class Devotional extends AppCompatActivity {
 
-    CardView sid;
+    CardView sid, tuljapur, pan, akka, gan, bar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devotional);
 
         sid = findViewById(R.id.siddheshwar);
+        tuljapur = findViewById(R.id.tuljapur);
+        pan = findViewById(R.id.pandharpur);
+        akka = findViewById(R.id.akkalkot);
+        gan = findViewById(R.id.ganangapur);
+        bar = findViewById(R.id.barshi);
 
         sid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Devo_info.class));
+                Intent i = new Intent(getApplicationContext(), Devo_info.class);
+                i.putExtra("dev", "sid");
+                startActivity(i);
+            }
+        });
+
+        tuljapur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Devo_info.class);
+                i.putExtra("dev", "tul");
+                startActivity(i);
             }
         });
     }
