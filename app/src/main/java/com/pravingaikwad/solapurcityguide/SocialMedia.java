c460fb6ef90848b01832bcab6449661a5ef4a72a
Akshay Jinde <akshayjinde6@gmail.com>
2020-08-24 20:16:10 +0530

    Emergency Activity

--- app/src/main/java/com/pravingaikwad/solapurcityguide/SocialMedia.java
+++ app/src/main/java/com/pravingaikwad/solapurcityguide/SocialMedia.java
@@ -2,13 +2,37 @@ package com.pravingaikwad.solapurcityguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SocialMedia extends AppCompatActivity {
+
+    LinearLayout fb, insta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+
+        fb = findViewById(R.id.facebook_page);
+        insta = findViewById(R.id.insta_page);
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/MH13-%E0%A4%86%E0%A4%AA%E0%A4%B2%E0%A4%82-%E0%A4%B8%E0%A5%8B%E0%A4%B2%E0%A4%BE%E0%A4%AA%E0%A5%81%E0%A4%B0-103069324856950/"));
+                startActivity(i);
+            }
+        });
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/mh13.aapla.solapur/"));
+                startActivity(i);
+            }
+        });
     }
 }
